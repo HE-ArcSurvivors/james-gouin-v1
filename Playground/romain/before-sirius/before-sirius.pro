2cb5d1eb372f39fe6a36fdbf0299abf69ce4c2b1
Romain Claret <romain.claret@rocla.ch>
2014-11-22 20:11:55 +0100

    Ajout du pixel art format 32x32 du joueur (penguin avec ruban rouge) Ajout du .gitignore pour les fichiers et dossier de build et .pro.user Menage dans le dossier: Images/Pixel Art Point chaud "Pre-Sirius": - Implementation des classes: GameBoard et Player - Le joueur ne peut pas sortir de la fenêtre de jeu (view) - Le joueur update son brush(texture) selon son orientation - Le joueur peut être placé sur la scene selon les coordonnées (case, case) - Le joueur peut se déplacer sur la carte avec w,a,s,d - La vue est facilement déplacable - Le jeu est scalable si on change la taille du joueur ou des cases - Développé sur un niveau fait main (à l'aide du logiciel Tiled) - Surment d'autres trucs que j'oublie :/ à vous les studios!

---
+++ Playground/romain/before-sirius/before-sirius.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-11-19T19:01:08
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = before-sirius
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+    player.cpp \
+    gameboard.cpp
+
+HEADERS  += \
+    player.h \
+    gameboard.h
+
+RESOURCES += \
+    images.qrc
---
+++ Playground/romain/before-sirius/gameboard.cpp
@@ -0,0 +1,85 @@
+#include "gameboard.h"
+
+Gameboard::Gameboard(QWidget *parent) : QWidget(parent)
+{
+    // Les Variables par default du jeu
+    windowTitle = tr("James Gouin et la Banane Sacrée");
+    windowSizeX = 640;
+    windowSizeY = 480;
+    viewStartPostionX = 1;
+    viewStartPostionY = 1;
+    viewPositionX = 640;
+    viewPositionY = 480;
+    startingPoint = QPoint (15,15); // 20x15
+    QString tutorialLevel3 = ":/images/test_map_tutoriel_3.png";
+
+    this->setWindowTitle(windowTitle);
+    this->setFixedSize(windowSizeX,windowSizeY);
+
+    mainScene = new QGraphicsScene(this);
+    playerView = new QGraphicsView(this);
+
+    //On crée la scene
+    QPixmap loadScene(tutorialLevel3);
+    mainScene->setBackgroundBrush(loadScene);
+    //mainScene->setSceneRect(viewStartPostionX,viewStartPostionY,viewPositionX,viewPositionX);
+
+    //Ce qui est dessous peut etre utilise si la map est plus grande que la vue
+    //playerView->setHorizontalScrollBarPolicy (Qt::ScrollBarAlwaysOff);
+    //playerView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    //playerView->setFocus();
+    //playerView->setFixedSize(640,480);
+    playerView->setSceneRect(viewStartPostionX,viewStartPostionY,viewPositionX,viewPositionX);
+
+    //On ajoute le joueur
+    player = new Player();
+    mainScene->addItem(player);
+    player->setPos(startingPoint.x()*gameSquares-gameSquares,startingPoint.y()*gameSquares-gameSquares);
+
+    //On position la vue
+    playerView->setScene(mainScene);
+
+}
+
+Gameboard::~Gameboard(){
+
+}
+
+//http://doc.qt.digia.com/4.6/qt.html#Key-enum
+void Gameboard::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_W)
+    {
+        // Determiner si le joueur sort de la vue
+        if (player->pos().y() > viewStartPostionY)
+        {
+            player->moveBy(0,-gameSquares);
+            player->setPlayerOrientation("up"); //definir l'orientation du joueur
+        }
+    }
+    if(event->key() == Qt::Key_S)
+    {
+        if (player->pos().y() <= viewPositionY-player->getPlayerSizeX()-8)
+        {
+            player->moveBy(0,gameSquares);
+            player->setPlayerOrientation("down");
+        }
+    }
+    if(event->key() == Qt::Key_A)
+    {
+        if (player->pos().x() > viewStartPostionY)
+        {
+            player->moveBy(-gameSquares,0);
+            player->setPlayerOrientation("left");
+        }
+    }
+    if(event->key() == Qt::Key_D)
+    {
+        if (player->pos().x() < viewPositionX-player->getPlayerSizeY())
+        {
+            player->moveBy(gameSquares,0);
+            player->setPlayerOrientation("right");
+        }
+    }
+    player->update();   //on recharge le painter
+}
