497aafec16680b3a216266e7179e4b032f90e44c
Steve Visinand <visinandst@gmail.com>
2014-12-01 17:01:46 +0100

    Collision : bloc déplaçables fonctionnels !

---
+++ Playground/steve/before-sirius/blocdeplacable.cpp
@@ -0,0 +1,5 @@
+#include "blocdeplacable.h"
+
+BlocDeplacable::BlocDeplacable()
+{
+}
---
+++ Playground/steve/before-sirius/blocdeplacable.h
@@ -0,0 +1,10 @@
+#ifndef BLOCDEPLACABLE_H
+#define BLOCDEPLACABLE_H
+
+class BlocDeplacable
+{
+public:
+    BlocDeplacable();
+};
+
+#endif // BLOCDEPLACABLE_H
